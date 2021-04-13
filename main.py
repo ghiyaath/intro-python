f8712f7cc72d48dbed29169016e57eeaa18b0f83
ghiyaath <ghiyaathwilliams2@gmail.com>
2021-04-13 09:30:00 +0200

    Added main.py file

---
+++ main.py
@@ -0,0 +1,10 @@
+# Task 2 Sample Solution.
+radius = input("Please enter the radius: ")
+
+_PI = 3.14
+area =_PI * int(radius)**2
+print("The area of a circle is: " + str(area))
+
+# Calculate the circumference
+circumference = 2 * _PI * int(radius)
+print("The circumference of a circle is: " + str (circumference))
